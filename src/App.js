47fd9d6e0386e026469c94b238e67bf6a66f5f27
김범서 <toraonine9@gmail.com>
2024-09-24 01:56:20 +0900

    add new features

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
-      setIsLoggedIn(true); // 토큰이 있으면 로그인 상태로 설정
+      setIsLoggedIn(true); // 토큰이 있으면 로그인 상태로 변경
     }
   }, []);
 
